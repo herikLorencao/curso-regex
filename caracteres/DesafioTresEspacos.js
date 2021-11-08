ed5a678a6da3f0012d63979fe317c2211d0c4c05
Herik S. Lorenção <herik.lorencao@itixti.com.br>
2021-11-08 09:33:29 -0300

    adicionando capítulo de caracteres

---
+++ caracteres/DesafioTresEspacos.js
@@ -0,0 +1,3 @@
+const texto = 'a   b';
+console.log(texto.match(/a   b/));
+console.log(texto.match(/a\s\s\sb/));
---
+++ caracteres/ProblemaPonto.js
@@ -0,0 +1,5 @@
+const texto = 'Bom\ndia';
+console.log(texto.match(/.../gi));
+console.log(texto.match(/..../gi)); // o ponto não engloba o \n
+
+// dotall - algumas linguagens tem uma flag /exp/s, mas JS não!
